b9b6f335dcd8f3058c943c41e6fd654f13a8f146
s1wy <siwy30a@wp.pl>
2019-11-28 15:35:17 +0100

    Add new files

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+ 
+
+function App() {
+  return (
+    <div style={{ margin: "0 auto", textAlign: "center" }}>
+      <img style={{ height: "100vh" }} src="/logo.png" alt="Logo" />
+    </div>
+  );
+}
+
+export default App;
