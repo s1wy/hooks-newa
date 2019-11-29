800841da728f5c7c74192af06a887b0ff459dd5c
s1wy <siwy30a@wp.pl>
2019-11-29 02:33:49 +0100

    Custom React Hooks

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import useFormValidation from './useFormValidation';
+import validationLogin from './validateLogin';
+
+const INITIAL_STATE = {
+  name: "",
+  email: "",
+  password: ""
+}
 
 function Login(props) {
-  return <div>Login</div>;
+  const {  handleSubmit, handleBlur, handleChange, values, errors, isSubmitting } = useFormValidation(INITIAL_STATE, validationLogin);
+  const [login, setLogin] = useState(true);
+
+  return (
+    <div>
+      <h2 className="mv3">{login ? "Login" : "Create Account"}</h2>
+      <form onSubmit ={handleSubmit}className="flex flex-column">
+        {!login && 
+          <input
+            onChange={handleChange}
+            value={values.name}
+            name="name"
+            type="text"
+            placeholder="Your name"
+            autoComplete="off"
+          />}
+        <input
+          onChange={handleChange}
+          onBlur={handleBlur} 
+          value={values.email}
+          name="email"
+          type="email"
+          className={errors.email && 'error-input'}
+          placeholder="Your email"
+          autoComplete="off"
+        />
+        {errors.email && <p className="error-text">{errors.email}</p>}
+        <input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.password}
+          name="password"
+          type="password"
+          className={errors.email && 'error-input'}
+          placeholder="Choose a secure password"
+          autoComplete="off"
+        />
+        {errors.password && <p className="error-text">{errors.password}</p>}
+        <div className="flex mt3">
+          <button type="submit" className="button pointer mr2" 
+            disabled={isSubmitting}
+            style={{background: isSubmitting ? "grey": "orange"}}
+          >
+            Submit
+          </button>
+          <button 
+            onClick={() => setLogin(prevLogin => !prevLogin)}
+            type="button" className="button pointer"
+          >
+            {login ? "need to create an account" : "Already have an account"}
+          </button>
+        </div>
+      </form>
+    </div>
+    );
 }
 
 export default Login;
--- src/components/Auth/useFormValidation.js
+++ src/components/Auth/useFormValidation.js
@@ -1,5 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function useFormValidation() {}
+function useFormValidation(initialState, validate) {
+  const [values, setValues] = useState(initialState);
+  const [errors, setErrors] = useState({});
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  useEffect(() => {
+    if (isSubmitting) {
+      const noErrors = Object.keys(errors).length === 0; 
+      if (noErrors) {
+        console.log('autenthicated', values);
+        setSubmitting(false);
+      } else {
+        setSubmitting(false);
+      }
+    }
+  }, [errors]);
+
+  function handleChange(event) {
+    event.persist();
+    setValues(previousValues => ({
+      ...previousValues,
+      [event.target.name]: event.target.value
+    }))
+  }
+
+  function handleBlur () {
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+    setSubmitting(true);
+    console.log({values})
+  }
+
+  return { handleSubmit, handleBlur, handleChange, values, errors, isSubmitting }
+}
 
 export default useFormValidation;
